587b8477718442174466b1f2121a3cc9083c2ae4
mac <mac@MacBook-Pro-mac.local>
2025-01-27 20:58:42 +0300

    Удалила лишние отступы

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by mac on 27.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
