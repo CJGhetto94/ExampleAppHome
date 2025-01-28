1eee8e2f46aa17e9324837a81b49a2b8580fb365
mac <mac@MacBook-Pro-mac.local>
2025-01-28 20:48:48 +0300

    Удалил лишние отступы

---
+++ ExampleAppHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleAppHome
+//
+//  Created by mac on 28.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
