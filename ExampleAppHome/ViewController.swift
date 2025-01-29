6f22121093a2868b135ac5a52e080f1a11edcc15
mac <mac@MacBook-Pro-mac.local>
2025-01-29 20:19:20 +0300

    Я создал свое первое приложение

--- ExampleAppHome/ViewController.swift
+++ ExampleAppHome/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let myName = "Artem"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("Меня зовут \(myName), Я создал свое первое приложение")
     }
 
 
