414bc4d254459a8b9cd906b362ef4dad1965e352
CJGhetto94 <cjghetto2013@gmail.com>
2025-02-19 20:32:54 +0300

    HM ex 11

--- ExampleAppHome/Helper.swift
+++ ExampleAppHome/Helper.swift
@@ -15,4 +15,29 @@ class Helper {
     func getListOfHumans() -> [User] {
         listOfHumans
     }
+    
+    func addFewHumans() {
+     // !!!!!!!!!!!!!!!!!!!!!
+    }
+}
+
+class UserRepository: Helper {
+    func returnArrayOfUsers() {
+        addHuman(User(loginUser: "CJGhetto",
+                                passwordUser: 1234,
+                                personalInformationOfUser: Person(firstName: "Artem", secondName: "Rudnev")))
+        addHuman(User(loginUser: "Raucher",
+                                passwordUser: 5678,
+                                personalInformationOfUser: Person(firstName: "Kirill", secondName: "Lemikin")))
+        addHuman(User(loginUser: "Nestor",
+                                passwordUser: 9876,
+                                personalInformationOfUser: Person(firstName: "Gleb", secondName: "Gaiger")))
+        addHuman(User(loginUser: "bigboi_slime",
+                                passwordUser: 1432,
+                                personalInformationOfUser: Person(firstName: "Vladislav", secondName: "Zamyatin")))
+        
+        for human in getListOfHumans() {
+            print(human.personalInformationOfUser.firstNameSecondName)
+        }
+    }
 }
--- ExampleAppHome/ViewController.swift
+++ ExampleAppHome/ViewController.swift
@@ -10,29 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     private let helper = Helper()
+    private let userRepository = UserRepository()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addPeopleHelper()
+        view.backgroundColor = UIColor(ciColor: .magenta)
+        view.alpha = CGFloat(0.9)
+        userRepository.returnArrayOfUsers()
+        helper.getListOfHumans()
     }
     
-    private func addPeopleHelper() {
-        helper.addHuman(User(loginUser: "CJGhetto",
-                             passwordUser: 1234,
-                             personalInformationOfUser: Person(firstName: "Artem", secondName: "Rudnev")))
-        helper.addHuman(User(loginUser: "Raucher",
-                             passwordUser: 5678,
-                             personalInformationOfUser: Person(firstName: "Kirill", secondName: "Lemikin")))
-        helper.addHuman(User(loginUser: "Nestor",
-                             passwordUser: 9876,
-                             personalInformationOfUser: Person(firstName: "Gleb", secondName: "Gaiger")))
-        helper.addHuman(User(loginUser: "bigboi_slime",
-                             passwordUser: 1432,
-                             personalInformationOfUser: Person(firstName: "Vladislav", secondName: "Zamyatin")))
-        
-        for human in helper.getListOfHumans() {
-            print(human.personalInformationOfUser.firstNameSecondName)
-        }
     }
-}
 
